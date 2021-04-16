31c15725cd45ab595e8b1c2535a0a356281c37e7
Juan Ahumada <juan.ahumada94@gmail.com>
2021-04-16 08:57:23 -0500

    Video Player Ready

---
+++ Assets/CustomTrackableEventHandler.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CustomTrackableEventHandler : DefaultTrackableEventHandler
+{
+  public bool isVideoEnabled;
+  [Space]
+  public VideoPlayer videoPlayer;
+  public RawImage rawImage;
+
+    protected override void Start()
+    {
+        base.Start();
+    StartCoroutine(PrepareVideo());
+    }
+
+  private IEnumerator PrepareVideo()
+  {
+    videoPlayer.Prepare();
+
+    while (!videoPlayer.isPrepared)
+    {
+      yield return new WaitForSeconds(.5f);
+    }
+
+    rawImage.texture = videoPlayer.texture;
+
+    isVideoEnabled = true;
+  }
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        if(isVideoEnabled) videoPlayer.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        if(isVideoEnabled) videoPlayer.Pause();
+    }
+}
